5044ffa5595b38997eb2dab1d974e025ad205bc2
deeeeno <ksh0512dog@gmail.com>
2022-04-29 23:31:32 +0900

    자료형 타입스크립트

--- src/data.ts
+++ src/data.ts
@@ -2,5 +2,44 @@ interface Human{
     name:string;
     age:number;
 }
+// array
 const member:{name:string,age:number}[] = [];
 member.push({name:'dino',age:27});
+
+// object
+function getUser(user:object){
+    console.log(user);
+}
+getUser([1,2,3]);
+
+// tuple
+let t:[string,boolean,number];
+t = ['1',true,1];
+
+// enum
+enum check{unchecked,checked,unknown}
+const Status = check.checked;
+console.log(Status);
+
+
+// any vs object
+function anyTest(obj:any){
+    console.log(obj.a);
+}
+anyTest({c:1});
+anyTest({a:1});
+
+function objectTest(obj:object){
+    console.log(obj.a);
+}
+objectTest({c:1});
+objectTest({a:1});
+
+
+// void, never
+function print(msg:string):void{
+    console.log(msg);
+}
+function neverTest():never{
+
+}
