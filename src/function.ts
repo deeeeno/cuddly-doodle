a8462efd3eef18e38d49a4bf8bdfee74d457a2ff
deeeeno <ksh0512dog@gmail.com>
2022-05-04 00:26:28 +0900

    feat : overloading 내용 추가
    overloading 예시 코드 추가
    notion : https://deeno.notion.site/Function-d6ae3ce56e254a879015feb707368e25

--- src/function.ts
+++ src/function.ts
@@ -1,2 +1,21 @@
 type Calculate = (a:number,b:number)=>number;
 const add:Calculate = (x,y)=>x+y;
+
+type Exception = {kind:string,trace:string};
+type Log = {
+		(level:string,message:string):void
+		(level:string,exception:Exception):void
+};
+const LOG:Log = (level,message)=>{
+		if(typeof message === "string") console.log(`[${level}] ${message}`);
+		else
+			console.log(`[${level}] ${message.kind} : ${message.trace}`);
+};
+
+type Encode = {
+    (message:string):void
+    (message:string,encode:string):void
+};
+const encode:Encode = (message,encode:string = 'utf-8')=>{
+    console.log(message, encode);
+};
