8f37a3fdf653873d547ec6e8f839f56b58cf4783
김성희 <kkrs0512@yna.co.kr>
2022-05-03 14:29:34 +0900

    타입 정리 완료

--- src/data.ts
+++ src/data.ts
@@ -1,16 +1,45 @@
-interface Human{
-    name:string;
-    age:number;
-}
-// array
+//타입 변수 선언
+const variable:number = 123;
+//물론 값에 대해서 추론도 가능하다.
+const whatisit = 123; //whatisit:number;
+//타입의 종류
+/*
+string
+number
+boolean
+null
+undefined
+object
+any
+unknown 
+등등...
+*/
+
+// Array
 const member:{name:string,age:number}[] = [];
+//or
+const members:Array<{name:string,age:number}> = [];
 member.push({name:'dino',age:27});
 
-// object
+// object - 원시자료형을 제외한 모든 것이 가능하다.
+// 단, object는 확실한 타입을 보장하기에 모르는 프로퍼티에 대해 사용할 수는 없다.
 function getUser(user:object){
     console.log(user);
 }
 getUser([1,2,3]);
+//any - 말그대로 모든 타입이 가능하다는 것이지. 이는 없는 프로퍼티에 대해서도 접근을 허용.
+
+// any vs object
+function anyTest(obj:any){
+    console.log(obj.a);
+}
+function objectTest(obj:object){
+    // console.log(obj.a);   error
+}
+objectTest({c:1});
+objectTest({a:1});
+anyTest({c:1});
+anyTest({a:1});
 
 // tuple
 let t:[string,boolean,number];
@@ -21,33 +50,45 @@ enum check{unchecked,checked,unknown}
 const Status = check.checked;
 console.log(Status);
 
-
-// any vs object
-function anyTest(obj:any){
-    console.log(obj.a);
-}
-anyTest({c:1});
-anyTest({a:1});
-
-function objectTest(obj:object){
-    // console.log(obj.a);
-}
-objectTest({c:1});
-objectTest({a:1});
-
-
 // void, never
-function print(msg:string):void{
-    console.log(msg);
+function error(msg:string):never{
+    throw new Error(msg);
 }
+function test(unk:string|number){
+    switch(typeof unk){
+        case 'string':
+            unk += 'a';
+            console.log('mouse over -> unk is string')
+            break;
+        case 'number':
+            unk += 1;
+            console.log('mouse over -> unk is number')
+            break;
+        default:
+            console.log('mouse over -> unk is never')
+            break;
+    }
+}
+function declareFunc(){   //declareFunc:void
+	while(true){}
+}
+const expressionFunc = ()=>{while(true){}}; //expressionFunc:never
 
-let a = "string";
-console.log(typeof a);
-let b:string = "string";
-console.log(typeof b);
+
 
 const dino : {
 	name:string;
 	age?:number;
 } = {name:'dino'};
 console.log(dino.name);
+
+type Man = {
+    readonly name:string;
+    age?:number;
+}
+const dave:Man = {name:'dave',age:12};
+dave.age  = 1000;
+//dave.name = 'a';  error
+//readonly -> 수정할 수 없어!
+const readonlyList:readonly number[] = [1,2,3];
+//readonlyList.push(1); error
